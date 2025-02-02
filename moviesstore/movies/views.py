89ce13cf2328418538163f17f27d6a8f2c76e8d0
Aumrita Maitra <aums3937@gmail.com>
2025-02-02 11:20:15 -0500

    chapter 5

---
+++ moviesstore/movies/views.py
@@ -0,0 +1,21 @@
+from django.shortcuts import render, redirect, get_object_or_404
+
+@login_required
+def edit_review(request, id, review_id, Review=None):
+    review = get_object_or_404(Review, id=review_id)
+    if request.user != review.user:
+        return redirect('movies.show', id=id)
+    if request.method == 'GET':
+        template_data = {}
+        template_data['title'] = 'Edit Review'
+        template_data['review'] = review
+        return render(request, 'movies/edit_review.html',
+            {'template_data': template_data})
+    elif request.method == 'POST' and
+    request.POST['comment'] != '':
+        review = Review.objects.get(id=review_id)
+        review.comment = request.POST['comment']
+        review.save()
+        return redirect('movies.show', id=id)
+    else:
+        return redirect('movies.show', id=id)
