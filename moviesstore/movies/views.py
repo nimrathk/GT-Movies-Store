8296c3de69220f89508d1f64c36fc7e292e483a2
Nimrath K <109793342+nimrathk@users.noreply.github.com>
2025-02-03 16:02:52 -0500

    took out a few comments

--- moviesstore/movies/views.py
+++ moviesstore/movies/views.py
@@ -2,9 +2,17 @@ from django.shortcuts import render, get_object_or_404, redirect
 from .models import Movie
 
 def index(request):
+    search_term = request.GET.get('search')
+    if search_term:
+        movies = Movie.objects.filter(name__icontains=search_term)
+    else:
+        movies = Movie.objects.all()
+    print("moviesie",list(Movie.objects.all()))
     template_data = {}
     template_data['title'] = 'Movies'
-    template_data['movies'] = Movie.objects.all()
+    template_data['movies'] = movies
+    print("template data,",template_data)
+    print("movies",list(movies))
     return render(request, 'movies/index.html',
                   {'template_data': template_data})
 
